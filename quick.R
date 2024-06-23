caf94d182d53df63933b658ff912d7eb8b400aad
AndrewMcMillanLandcare <mcmillanan@landcareresearch.co.nz>
2024-06-24 08:42:03 +1200

    recounting flowing trees

---
+++ quick.R
@@ -0,0 +1,15 @@
+
+# 
+# util_script = paste0("C:/Users/McMillanAn/OneDrive - MWLR/Code/R/utils/", "mcm_sp_utils.R")
+# source(util_script)
+# 
+
+# RC = ea_load("RC")
+# mapview(RC)
+# 
+
+library(mapview)
+
+
+options(viewer = NULL)
+mapview(breweries)
